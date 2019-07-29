e590932e5a4d1fad89f820ae28b8fc47fe4a80e4
rarrunategu1 <parker.rose@ymail.com>
2019-07-29 19:17:55 -0400

    Using a class with an attribute and a method and then creating an object to call / use it in the main method

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -2,11 +2,22 @@
 
 namespace ClassesAndObjects
 {
+    public class Person
+    {
+        public string Name;
+        
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person person = new Person();
+            person.Name = "Rose";
+            person.Introduce("Bill");
         }
     }
 }
