361ac03ec93b0fa6c2832b51de2c70c2484327f3
rarrunategu1 <parker.rose@ymail.com>
2019-07-30 18:58:14 -0400

    Overloading method example

---
+++ ClassesAndObjects/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    //practice with Methods
+    class Point
+    {   
+        //set fields
+        public int X;
+        public int Y;
+        
+        //initialize it
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //method to move this point to a different location
+        public void Move(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        //overload this method
+        public void Move(Point newLocation)
+        {
+            this.X = newLocation.X;
+            this.Y = newLocation.Y;
+        }
+    }
+}
--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -22,16 +22,29 @@ namespace ClassesAndObjects
     {
         static void Main(string[] args)
         {
+            //classes an objects from above
             Person person = Person.Parse("Alex");
             person.Introduce("Bill");
-
+            
+            //constructors on Customer.cs and Order.cs
             Customer customer = new Customer(1, "Shawn");
             
             var order = new Order();
             customer.Orders.Add(order);
-            
+
             Console.WriteLine(customer.Id);
             Console.WriteLine(customer.Name);
+
+            //methods on Point.cs
+            //overloading
+            var point = new Point(10, 20);
+            point.Move(new Point(40, 60));
+            Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+            
+            point.Move(100, 200);
+            Console.WriteLine("Point is at {0}, {1}", point.X, point.Y);
+
+
         }
     }
 }
